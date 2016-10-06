d663b1ba3db843ba99e86f5e58d47162289ba231
vesaliE <ellangovesali@gmail.com>
2016-10-06 14:22:15 +0800

    Added sql for postgresql

---
+++ Login.php
@@ -0,0 +1,59 @@
+<html>
+<head> <title>WELCOME!</title> </head>
+
+<body>
+<table>
+<tr> <td colspan="2" style="background-color:#FFA500;">
+<h1>Please enter your email and password</h1>
+</td> </tr>
+
+<?php
+$dbconn = pg_connect("host=localhost port=5432 dbname=Sharing user=postgres
+password=12345678")
+    or die('Could not connect: ' . pg_last_error());
+?>
+
+<tr>
+<td style="background-color:#eeeeee;">
+<form>
+
+	Email: <input type="text" name="Email" id="Email">
+    Password: <input type="text" name="Password" id="Password">
+
+    <input type="submit" name="formSubmit" value="Login" >
+        
+</form>
+	
+	<?php
+if(isset($_GET['formSubmit'])) 
+{
+    $query = "SELECT email, password FROM users WHERE email = '".$_GET['Email']."' AND password = '".$_GET['Password']."'";
+    
+    echo "<b>SQL:   </b>".$query."<br><br>";
+    $result = pg_query($query) or die('Search failed: ' . pg_last_error());
+
+
+    if (pg_num_rows($result) == 1) {
+    	echo "true";
+    	header("Location:BrowsingPage.php");
+    	//Change page
+    	exit;
+    } else {
+    	echo "Wrong Password!";
+    }
+    
+    pg_free_result($result);
+}
+?>
+</td> </tr>
+	<?php
+	pg_close($dbconn);
+	?>
+
+<tr>
+<td colspan="2" style="background-color:#FFA500; text-align:center;"> Copyright &#169; CS2102
+</td> </tr>
+</table>
+
+</body>
+</html>
---
+++ SharingProjectSchema.sql
@@ -0,0 +1,86 @@
+CREATE TABLE user(
+	email VARCHAR(64) PRIMARY KEY,
+	username VARCHAR(16) NOT NULL UNIQUE,  
+	password VARCHAR(16) NOT NULL, 
+	location VARCHAR(64) NOT NULL
+); 
+CREATE TABLE object(
+	productID INT PRIMARY KEY, 
+	category VARCHAR(64) (CHECK category='Electronics' OR category='Book' OR category='Clothing', category='Services') NOT NULL, 
+	itemName VARCHAR(64) NOT NULL,
+	price  FLOAT (CHECK price >=0) NOT NULL, 
+	description VARCHAR(256), 
+	availability BOOLEAN NOT NULL, 
+	date DATE,
+	owner VARCHAR(64) REFERENCES user(email)
+); 
+CREATE TABLE loan(
+	loanID VARCHAR(256) PRIMARY KEY, 
+	borrowDate DATE NOT NULL, 
+	returnDate DATE NOT NULL, 
+	owner VARCHAR(64) REFERENCES user(email), 
+	borrower VARCHAR(64) REFERENCES user(email) CHECK(borrower <> owner), 
+	productID VARCHAR(64) REFERENCES object(productID), 
+	UPDATE ON CASCADE, 
+	DELETE ON CASCADE
+); 
+
+CREATE TABLE auction(
+	auctionID VARCHAR(256) PRIMARY KEY, 
+	deadline DATE NOT NULL,
+	winner VARCHAR(64) REFERENCES user(email), 
+	owner VARCHAR(64) REFERENCES object(owner)
+);
+
+CREATE TABLE bid(
+	price FLOAT NOT NULL, 
+	user REFERENCES user(email), 
+	auctionID VARCHAR(256) REFERENCES auction(auctionID),
+	PRIMARY KEY(price, user, auctionID)
+);
+
+/*
+//POSTGRESQL: working sql code 
+CREATE TABLE "user" (
+	email VARCHAR(64) PRIMARY KEY,
+	username VARCHAR(16) NOT NULL UNIQUE,  
+	password VARCHAR(16) NOT NULL, 
+	location VARCHAR(64) NOT NULL
+); 
+
+CREATE TABLE object (
+	productID INT PRIMARY KEY, 
+  owner VARCHAR(64) REFERENCES "user" (email) UNIQUE , 
+	category VARCHAR(64) NOT NULL, CHECK (category='Electronics' OR category='Book' OR category='Clothing' OR category='Services'), 
+	itemName VARCHAR(64) NOT NULL,
+	price  FLOAT NOT NULL, CHECK (price >=0), 
+	description VARCHAR(256), 
+	availability BOOLEAN NOT NULL, 
+	date DATE
+); 
+
+CREATE TABLE loan (
+	loanID VARCHAR(256) PRIMARY KEY, 
+	borrowDate DATE NOT NULL, 
+	returnDate DATE NOT NULL, 
+	owner VARCHAR(64) REFERENCES "user" (email), 
+	borrower VARCHAR(64) REFERENCES "user" (email) ON UPDATE CASCADE ON DELETE CASCADE,  CHECK(borrower <> owner), 
+	productID INT REFERENCES object(productID) ON UPDATE CASCADE ON DELETE CASCADE 
+); 
+
+CREATE TABLE auction (
+	auctionID VARCHAR(256) PRIMARY KEY, 
+	deadline DATE NOT NULL,
+	winner VARCHAR(64) REFERENCES "user" (email), 
+	owner VARCHAR(64) REFERENCES object(owner)
+);
+
+CREATE TABLE bid (
+	price FLOAT NOT NULL, 
+	userID VARCHAR(64) REFERENCES "user" (email), 
+	auctionID VARCHAR(256) REFERENCES auction(auctionID),
+	PRIMARY KEY(price, userID, auctionID)
+);
+*/
+
+
