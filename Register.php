769fcfb6bc7e4ce11da29e63a0779e3f82ed4b35
sherinatoh <sherinatohh@gmail.com>
2016-10-25 10:37:38 +0800

    Update Register.php

--- Register.php
+++ Register.php
@@ -2,59 +2,65 @@
 <head> <title>WELCOME!</title> </head>
 
 <body>
-<table>
-<tr> <td colspan="2" style="background-color:#FFA500;">
-<h1>Please enter a valid email and password and choose a username</h1>
-</td> </tr>
+    <table>
+        <tr> <td colspan="2" style="background-color:#6666C1;">
+            <h1 style="font-size:70px; color:#FFF8DC">Registration</h1>
+        </td> </tr>
 
-<?php
-$dbconn = pg_connect("host=localhost port=5432 dbname=Sharing user=postgres
-password=12345678")
-    or die('Could not connect: ' . pg_last_error());
-?>
+        <?php
+        $dbconn = pg_connect("host=localhost port=5432 dbname=Sharing user=postgres password=10220911")
+        or die('Could not connect: ' . pg_last_error());
+        ?>
 
-<tr>
-<td style="background-color:#eeeeee;">
-<form>
+        <tr>
+            <td style="background-color:#eeeeee;">
+                <form>
+                    <div style="width:300px">
 
-	Username: <input type="text" name="Username" id="Username">
-    Email: <input type="text" name="Email" id="Email">
-    Password: <input type="text" name="Password" id="Password">
-    
-    <input type="submit" name="formSubmit" value="Create Account" >
-    <input type="button" value="Login Now!" onClick="document.location.href='Login.php'"/>
-</form>
+                       <label style="color:#2D2D2D; width:250px; display: block"><span style="position:relative;">Username:</span><input style="float:right; display:inline" type="text" name="Username" id="Username"></label><br>
+                      
+                       <label style="color:#2D2D2D; width:250px; display: block"><span style="position:relative;">Email: </span><input style="float:right; display:inline" type="text" name="Email" id="Email"></label> <br>
 
-<?php
+                       <label style="color:#2D2D2D; width:250px; display: block"><span style="position:relative;">Password: </span><input style="float:right; display:inline" type="text" name="Password" id="Password"></label> <br>
 
-if(isset($_GET['formSubmit'])) 
-{
-    $query = "INSERT INTO users VALUES('".$_GET['Email']."','".$_GET['Username']."','".$_GET['Password']."')";
-    
-    //echo "<b>SQL:   </b>".$query."<br><br>";
+                       <label style="color:#2D2D2D; width:250px; display: block"><span style="position:relative;">Location: </span><input style="float:right; display:inline" type="text" name="Location" id="Location"> </label><br>
 
-    $result = pg_query($query);
+                   </div>
+
+                   <input type="submit" name="formSubmit" value="Create Account" >
+                   <input type="button" value="Login Now!" onClick="document.location.href='Login.php'"/>
+               </form>
+
+               <?php
+
+               if(isset($_GET['formSubmit'])) 
+               {
+                $query = "INSERT INTO member VALUES('".$_GET['Email']."','".$_GET['Username']."','".$_GET['Password']."','".$_GET['Location']."')";
+
+                echo "<b>SQL:   </b>".$query."<br><br>";
+
+                $result = pg_query($query);
     //For now it only shows error message in SQL but does not explain what is causing the error to
     //the users
 
-    if (!$result) {
-    	echo "Email/Username already taken, please try something else!";
-    } else {
-    	echo "Account created successfully, please click on Login Now to login!";
-    }
+                if (!$result) {
+                   echo "Email/Username already taken, please try something else!";
+               } else {
+                   echo "Account created successfully, please click on Login Now to login!";
+               }
 
-    pg_free_result($result);
-}
-?>
+               pg_free_result($result);
+           }
+           ?>
 
-</td> </tr>
-<?php
-pg_close($dbconn);
-?>
-<tr>
-<td colspan="2" style="background-color:#FFA500; text-align:center;"> Copyright &#169; CS2102
-</td> </tr>
-</table>
+       </td> </tr>
+       <?php
+       pg_close($dbconn);
+       ?>
+       <tr>
+        <td colspan="2" style="background-color:#6666C1; color:#FFF8DC; text-align:center; margin-top: 10px; padding: 10px"> Copyright &#169; VYMMS
+        </td> </tr>
+    </table>
 
 </body>
 </html>
