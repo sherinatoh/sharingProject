b312bb0079d42c36063d4f5af2dafebb5fcd06a4
sherinatoh <sherinatohh@gmail.com>
2016-10-25 10:36:49 +0800

    Update FirstPage

--- FirstPage.php
+++ FirstPage.php
@@ -3,8 +3,8 @@
 
 <body>
 <table>
-<tr> <td colspan="2" style="background-color:#FFA500;">
-<h1> Welcome to Stuff Sharing Services!</h1>
+<tr> <td colspan="2" style="background-color:#6666C1;">
+<h1 style="color:#FFF8DC; font-size: 70px"> Welcome to VYMMS Services!</h1>
 </td> </tr>
 
 
@@ -12,17 +12,17 @@
 <td style="background-color:#eeeeee;">
 <form>
 
-        <h3> Please select Login if you are already registered </h3>
-        <h3> Or Register if you are a new user </h3>
-        <input type="button" value="Login" onClick="document.location.href='Login.php'"/>
-        <input type="button" value="Register" onClick="document.location.href='Register.php'"/>
+        <h3 style="font-size:50px"> Please select Login if you are already registered </h3>
+        <h3 style="font-size:50px"> Or Register if you are a new user </h3>
+        <input style="background-color:#6666C1; color:#FFF8DC; padding:5px 10px; border-radius: 12px; font-size:30px; width:50%; float:left; margin-bottom:10px" type="button" value="Login" onClick="document.location.href='Login.php'"/>
+        <input style="background-color:#6666C1; color:#FFF8DC; padding:5px 10px; border-radius: 12px; font-size:30px; width:50%; float:left; margin-bottom:50px" type="button" value="Register" onClick="document.location.href='Register.php'"/>
 </form>
 
 
 </td> </tr>
 
 <tr>
-<td colspan="2" style="background-color:#FFA500; text-align:center;"> Copyright &#169; CS2102
+<td colspan="2" style="background-color:#6666C1; color:#FFF8DC; text-align:center; margin-top: 10px; padding: 10px"> Copyright &#169; VYMMS
 </td> </tr>
 </table>
 
