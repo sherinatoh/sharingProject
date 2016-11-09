a885f2e1f795be689ff453740faf774a355aa0fe
Murray Chen <mchen92@illinois.edu>
2016-11-09 17:53:27 +0800

    upload images

--- upload.php
+++ upload.php
@@ -1,33 +1,39 @@
 <?php
-$dbconn = pg_connect("host=localhost port=5432 dbname=Sharing user=postgres password=12345678")
+$dbconn = pg_connect("host=localhost port=5432 dbname=Sharing user=postgres password=cs2102")
 or die('Could not connect: HERE' . pg_last_error());
 $target_dir = "img/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-;
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["formSubmit"])) {
-
+    $fileUploaded = is_uploaded_file($_FILES["fileToUpload"]["tmp_name"]);
+    echo $fileUploaded. "\n";
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
+    if($check !== false || !is_uploaded_file($_FILES["fileToUpload"]["tmp_name"])) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
-        $target_file = $target_dir . crc32($_POST['category'].$_POST['itemName'].$_POST['description'].$_POST['availableDate']."jpg");
-        $currObjectID = crc32($_POST['category'].$_POST['itemName'].$_POST['description'].$_POST['availableDate']);
 
-        $query = "INSERT INTO object VALUES('".$currObjectID."', '".$_POST['category']."','".$_POST['itemName']."','".$_POST['description']."','".$_POST['price']."','".$_POST['availableDate']."','".$_POST['availability']."')";
+        $currObjectID = round(crc32($_POST['category'].$_POST['itemName'].$_POST['description']) / 15);
+
+        if($fileUploaded){
+            $target_file = $currObjectID . ".jpg";
+        } else {
+            $target_file = "default.jpg";
+            $uploadOK = 0;
+        }
+
+        $query = "INSERT INTO object VALUES('".$currObjectID."',
+        '".$_POST['category']."','".$_POST['itemName']."','".$_POST['description']."',
+        '".$_POST['price']."','".$_POST['availableDate']."','".$_POST['availability']."','".$target_file."')";
 //echo "<b>SQL:   </b>".$query."<br><br>";
         echo $query . "\n";
-        echo "FUK";
         $result = pg_query($query) or die('Query failed: ' . pg_last_error());
         echo $currObjectID;
         if(!result){
             echo "Please enter all fields";
             $uploadOk = 0;
-        }
-        else{
-
         }
 
     } else {
@@ -36,28 +42,29 @@ if(isset($_POST["formSubmit"])) {
     }
 }
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+if(!$fileUploaded || ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                    && $imageFileType != "JPG" && $imageFileType != "PNG" && $imageFileType != "JPEG")){
     echo "Sorry, only JPG, JPEG, PNG files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
+    header("Location: browsing.php");
+    exit;
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        header("Location:browsing.php");
+        header("Location: browsing.php");
         exit;
     } else {
         echo "Sorry, there was an error uploading your file.";
+        header("Location: browsing.php");
+        exit;
     }
 }
-
+echo "what the fuck7";
 ?>
