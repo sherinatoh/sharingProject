b269b01a7982698f6eba8d30b5f310548aa4107d
sherinatoh <sherinatohh@gmail.com>
2016-11-09 18:55:31 +0800

    Update Upload.php

--- upload.php
+++ upload.php
@@ -1,11 +1,17 @@
 <?php
-$dbconn = pg_connect("host=localhost port=5432 dbname=Sharing user=postgres password=cs2102")
+$dbconn = pg_connect("host=localhost port=5432 dbname=Sharing user=postgres password=10220911")
 or die('Could not connect: HERE' . pg_last_error());
+ob_start();
+session_start();
+if(!isset($_SESSION['user'])){
+    echo "Please login <a href='FirstPage.php'>here</a>.";
+    header("Location: /Login.php");
+    exit();
+}
 $target_dir = "img/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
 // Check if image file is a actual image or fake image
 if(isset($_POST["formSubmit"])) {
     $fileUploaded = is_uploaded_file($_FILES["fileToUpload"]["tmp_name"]);
@@ -14,19 +20,17 @@ if(isset($_POST["formSubmit"])) {
     if($check !== false || !is_uploaded_file($_FILES["fileToUpload"]["tmp_name"])) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
-
         $currObjectID = round(crc32($_POST['category'].$_POST['itemName'].$_POST['description']) / 15);
-
         if($fileUploaded){
             $target_file = $currObjectID . ".jpg";
         } else {
             $target_file = "default.jpg";
             $uploadOK = 0;
         }
-
+        $date = date('Y-m-d');
         $query = "INSERT INTO object VALUES('".$currObjectID."',
         '".$_POST['category']."','".$_POST['itemName']."','".$_POST['description']."',
-        '".$_POST['price']."','".$_POST['availableDate']."','".$_POST['availability']."','".$target_file."')";
+        '".$_POST['price']."','".$date."','TRUE','".$target_file."','".$_SESSION['user']."')";
 //echo "<b>SQL:   </b>".$query."<br><br>";
         echo $query . "\n";
         $result = pg_query($query) or die('Query failed: ' . pg_last_error());
@@ -35,19 +39,16 @@ if(isset($_POST["formSubmit"])) {
             echo "Please enter all fields";
             $uploadOk = 0;
         }
-
     } else {
         echo "File is not an image.";
         $uploadOk = 0;
     }
 }
-
-
 // Allow certain file formats
 if(!$fileUploaded || ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                    && $imageFileType != "JPG" && $imageFileType != "PNG" && $imageFileType != "JPEG")){
+    && $imageFileType != "JPG" && $imageFileType != "PNG" && $imageFileType != "JPEG")){
     echo "Sorry, only JPG, JPEG, PNG files are allowed.";
-    $uploadOk = 0;
+$uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
